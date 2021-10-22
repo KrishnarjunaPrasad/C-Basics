a580b4335ac7754c571ca6c756bd45875e3297f2
KrishnarjunaPrasad <92074715+KrishnarjunaPrasad@users.noreply.github.com>
2021-10-22 10:18:25 +0530

    Add files via upload

---
+++ All Escape Sequences.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	printf("HELLO WORLD\n");
+	printf("I am\tKrishnarjuna prasad\n");
+	printf("I am from andhra pradesh\r I came\n");
+	printf("\\ is for making the slash in c\n");
+	printf("I am from \"kathipudi\"");
+}
+
