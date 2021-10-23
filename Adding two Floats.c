8dda8031c3d6de2179a1cd4fa5c75f2a09ca9f06
KrishnarjunaPrasad <92074715+KrishnarjunaPrasad@users.noreply.github.com>
2021-10-23 10:55:55 +0530

    Add files via upload

---
+++ Adding two Floats.c
@@ -0,0 +1,14 @@
+// Adding two decimal numbers
+#include<stdio.h>
+void main()
+{
+	// variable decleartion
+	float number1,number2,sum;
+	//intilasation
+	number1 = 18.2;
+	number2 = 22.2;
+	// caluclation
+	sum = number1+number2;
+	//output
+	printf("sum of %f and %f is %f",number1,number2,sum);
+}
