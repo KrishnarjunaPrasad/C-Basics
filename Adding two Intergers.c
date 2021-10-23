838ebf468deede9cc91e0718cc2370e5962b6312
KrishnarjunaPrasad <92074715+KrishnarjunaPrasad@users.noreply.github.com>
2021-10-23 10:56:46 +0530

    Add files via upload

---
+++ Adding two Intergers.c
@@ -0,0 +1,14 @@
+// Adding two intergers and print the sum
+#include<stdio.h>
+void main()
+{
+	//variable decleration
+	int number1,number2,sum;
+	
+	//Intilaasation
+	number1 = 30;
+	number2 = 20;
+	// output
+	sum = number1+number2;
+	printf("sum of %i and %i is %i", number1,number2,sum);
+}
