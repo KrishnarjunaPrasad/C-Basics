a2e4f7cae5c23168aff675915c22e714392b56ce
KrishnarjunaPrasad <92074715+KrishnarjunaPrasad@users.noreply.github.com>
2021-10-23 10:11:21 +0530

    Add files via upload

---
+++ Data types and variables Basics.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	// variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	//intialasation
+	age = 15;
+	cgpa = 9.3;
+	initial = 'T';
+	// output
+	printf(" Age of the student %d\n", age);
+	printf(" Cgpa of the student %f\n", cgpa);
+	printf(" Sur name of the student %c", initial);
+}
